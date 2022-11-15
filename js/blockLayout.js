0ccc59687cfc4640e91450e6f05fc7e28e35492f
cqb13 <cqb13.main@gmail.com>
2022-11-15 13:27:00 -0500

    🖋 Custom colors

--- js/blockLayout.js
+++ js/blockLayout.js
@@ -1,27 +1,32 @@
-const button = document.getElementById("generate");
+const backgroundColorPicker = document.getElementById("background-color");
 const blockColorPicker = document.getElementById("block-color");
 const blocks = document.getElementsByClassName("block");
+const button = document.getElementById("generate");
 var blockAmount;
-blockAmount = generate();
+generate();
 
 button.addEventListener("click", function () {
-  blockAmount = generate();
-  for (var i = 0; i < blockAmount; i++) {
-    createBlocks();
-  }
+  generate();
 });
-
-function generate() {
-  var blockAmount = Math.floor(Math.random() * 10) + 1;
-  document.getElementById("block-container").innerHTML = "";
-  return blockAmount;
-}
 
 blockColorPicker.addEventListener("change", function () {
   for (var i = 0; i < blocks.length; i++) {
     blocks[i].style.backgroundColor = blockColorPicker.value;
   }
 });
+
+backgroundColorPicker.addEventListener("change", function () {
+  document.body.style.background =
+    "linear-gradient(180deg, #f9f9f9, " + backgroundColorPicker.value + ")";
+});
+
+function generate() {
+  var blockAmount = Math.floor(Math.random() * 10) + 1;
+  document.getElementById("block-container").innerHTML = "";
+  for (var i = 0; i < blockAmount; i++) {
+    createBlocks();
+  }
+}
 
 function createBlocks() {
   var block = document.createElement("div");
